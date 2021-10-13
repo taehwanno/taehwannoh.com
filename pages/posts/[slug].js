67d1bc48a91a12579d852c2ca443617a0833305f
Taehwan Noh <taehwanno.dev@gmail.com>
2021-10-14 01:47:31 +0900

    fix: update title

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -29,9 +29,7 @@ export default function Post({ post, morePosts, preview }) {
           <>
             <article>
               <Head>
-                <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
-                </title>
+                <title>{post.title} | taehwannoh.com</title>
                 <meta property="og:image" content={post.coverImage.url} />
               </Head>
               <PostHeader
