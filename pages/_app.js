358fed4c4c65cc502a24b91247269336be653186
Taehwan Noh <taehwanno.dev@gmail.com>
2022-06-14 01:20:39 +0900

    fix: add ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,45 @@
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import { useEffect } from 'react';
+
+import * as gtag from '../lib/gtag';
 import '../styles/index.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-37SF1PGR91"
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-37SF1PGR91', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
