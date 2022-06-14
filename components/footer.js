d2ad068dcd1b04cf5dcca5e13b283a23784e036f
Taehwan Noh <taehwanno.dev@gmail.com>
2022-06-14 22:08:38 +0900

    fix: improve footer

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,4 @@
 import Container from './container';
-import { EXAMPLE_PATH } from '../lib/constants';
 
 export default function Footer() {
   return (
@@ -17,41 +16,41 @@ export default function Footer() {
             >
               Contact Me
             </a>
-            <a
-              className="ml-3 mr-5"
-              href="https://github.com/taehwanno"
-              target="_blank"
-              rel="noopener"
-            >
-              <img
-                className="w-5 h-5"
-                src="https://user-images.githubusercontent.com/7760903/53615236-5d183b80-3c20-11e9-8665-40f837e91ccf.png"
-                alt="GitHub"
-              />
-            </a>
-            <a
-              className="mr-5"
-              href="https://medium.com/@taehwannoh"
-              target="_blank"
-              rel="noopener"
-            >
-              <img
-                className="w-5 h-5"
-                src="https://user-images.githubusercontent.com/7760903/53615395-052e0480-3c21-11e9-8b3b-29e064acc35b.png"
-                alt="Medium"
-              />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/taehwannoh"
-              target="_blank"
-              rel="noopener"
-            >
-              <img
-                className="w-6 h-5"
-                src="https://user-images.githubusercontent.com/7760903/53615700-89cd5280-3c22-11e9-9657-0b238c9ed3ed.png"
-                alt="Linkedin"
-              />
-            </a>
+            <div className="flex flex-row lg:ml-3 gap-x-6">
+              <a
+                href="https://github.com/taehwanno"
+                target="_blank"
+                rel="noopener"
+              >
+                <img
+                  className="w-5 h-5"
+                  src="https://user-images.githubusercontent.com/7760903/53615236-5d183b80-3c20-11e9-8665-40f837e91ccf.png"
+                  alt="GitHub"
+                />
+              </a>
+              <a
+                href="https://medium.com/@taehwannoh"
+                target="_blank"
+                rel="noopener"
+              >
+                <img
+                  className="w-5 h-5"
+                  src="https://user-images.githubusercontent.com/7760903/53615395-052e0480-3c21-11e9-8b3b-29e064acc35b.png"
+                  alt="Medium"
+                />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/taehwannoh"
+                target="_blank"
+                rel="noopener"
+              >
+                <img
+                  className="w-6 h-5"
+                  src="https://user-images.githubusercontent.com/7760903/53615700-89cd5280-3c22-11e9-9657-0b238c9ed3ed.png"
+                  alt="Linkedin"
+                />
+              </a>
+            </div>
           </div>
         </div>
         <div className="py-4">© Taehwan Noh. All rights reserved.</div>
