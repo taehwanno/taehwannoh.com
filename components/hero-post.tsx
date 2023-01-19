3ce8f68b07beefc4ad06fcd139ba421786557e7a
Taehwan Noh <taehwanno.dev@gmail.com>
2023-01-19 13:04:36 +0900

    fix: change title position in hero post (#39)

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -20,11 +20,11 @@ export default function HeroPost({
   slug,
 }: HeroPostProps) {
   return (
-    <section>
+    <section className="flex flex-col md:flex-col-reverse">
       <div className="mb-8 md:mb-16">
         <CoverImage title={title} slug={slug} url={coverImage.url} />
       </div>
-      <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28">
+      <div className="md:grid mb-8 md:mb-18">
         <div>
           <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
             <Link
@@ -35,9 +35,6 @@ export default function HeroPost({
               {title}
             </Link>
           </h3>
-          <div className="mb-4 md:mb-0 text-lg">
-            <DateComponent dateString={date} />
-          </div>
         </div>
         <div>
           <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
