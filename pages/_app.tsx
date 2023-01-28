c6647b3e140c89961d063863f6721369b86fa38b
Taehwan Noh <taehwanno.dev@gmail.com>
2023-01-28 15:56:58 +0900

    feat: google adsense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -38,6 +38,11 @@ function MyApp({ Component, pageProps }: AppProps) {
           `,
         }}
       />
+      <Script
+        strategy="afterInteractive"
+        crossOrigin="anonymous"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2544441362435885"
+      />
       <Component {...pageProps} />
     </>
   );
